987d71e7b2a2f557988ccf68f880a0aa040fbbd1
thegergo02 <thegergo02@protonmail.com>
2018-12-23 19:55:17 +0100

    Comment insertion-sort.py

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,14 +1,14 @@
-import sys
+import sys #Import sys
 
-sys.argv.pop(0)
-A = list(map(int, sys.argv))
+sys.argv.pop(0) #We delete the first index, we don't need the script name.
+A = list(map(int, sys.argv)) #We create a list, and map the arguments (that we get in string) to ints.
 
-print(A)
-for j in range(len(A)):
-	key = A[j]
-	i = j - 1
-	while i >= 0 and A[i] > key:
-		A[i + 1] = A[i]
-		i = i - 1
-	A[i + 1] = key
-print(A)
+print(A) #We print out our arguments.
+for j in range(len(A)): #We iterate over our numbers.
+	key = A[j] #We set key to our current number.
+	i = j - 1 #We create a variable to store the previous number's index.
+	while i >= 0 and A[i] > key: #While the 'i' variable isn't 0 or greater, and the previous number is greater than our number, do this loop.
+		A[i + 1] = A[i] #Set our number to the previous number.
+		i = i - 1 #Decrease the 'i' variable.
+	A[i + 1] = key #On 'i' + 1 index, we change the value to our current value.
+print(A) #We print the sorted array/list.
